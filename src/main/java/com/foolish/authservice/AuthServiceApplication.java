b01428629248f938b7e89ce9d081b32e440ad61b
MaivMinh <maivanminh.se@gmail.com>
2024-11-28 23:05:08 +0700

    Complete the connection between api gateway and service-discovery, and mapping routes to auth service

--- src/main/java/com/foolish/authservice/AuthServiceApplication.java
+++ src/main/java/com/foolish/authservice/AuthServiceApplication.java
@@ -3,15 +3,14 @@ package com.foolish.authservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
-import static org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO;
 
 @SpringBootApplication
 @EnableJpaRepositories("com.foolish.authservice.repository")
 @EntityScan("com.foolish.authservice.model")
-@EnableSpringDataWebSupport(pageSerializationMode = VIA_DTO)
+@EnableDiscoveryClient
 public class AuthServiceApplication {
 
   public static void main(String[] args) {
