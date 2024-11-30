498e0ac05a76542c791943a3c420fecec961dc20
MaivMinh <maivanminh.se@gmail.com>
2024-11-30 14:05:08 +0700

    Rename AuthGrpcServer to AuthServiceGrpcServer

--- src/main/java/com/foolish/authservice/services/AuthServiceGrpcServer.java
+++ src/main/java/com/foolish/authservice/services/AuthServiceGrpcServer.java
@@ -20,7 +20,7 @@ import javax.annotation.Nullable;
 @GrpcService
 @Service
 @AllArgsConstructor
-public class AuthGrpcServer extends AuthServiceGrpc.AuthServiceImplBase {
+public class AuthServiceGrpcServer extends AuthServiceGrpc.AuthServiceImplBase {
   private final AccountService accountService;
 
   @Bean
